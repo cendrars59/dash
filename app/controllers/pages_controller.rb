ae7d8755b9f586eecb8a2c4464cd87f18fab2793
cendrars59 <cyr.leroux.dev@moinslequart.net>
2016-07-29 21:21:36 +0200

    Beg Welcome

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+
+  # method for the homepage when user is not connected
   def home
   end
+
+  # method for the homepage when user is not connected
+  def welcome
+  end
+
+
+
 end
