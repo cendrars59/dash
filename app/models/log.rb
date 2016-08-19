6f2c5e99881386122ea8b84ea31127cc22a4ae55
cyril leroux <cyr.leroux.dev@moinslequart.net>
2016-08-19 21:46:52 +0200

    Log creation with code generation failed

--- app/models/log.rb
+++ app/models/log.rb
@@ -10,4 +10,22 @@ class Log < ApplicationRecord
   belongs_to :log_type
   belongs_to :milestone
 
+#############################################################################
+# Log business rules and controls
+#############################################################################
+
+
+########### When creating or updating log
+
+  before_create :set_code
+
+
+  private
+
+  def set_code
+    self.code = self.log_type.code +(1..9999).to_a.shuffle.to_s.rjust(4,'0')
+  end
+
+
+
 end
